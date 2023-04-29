2dbbd10ff51e0752b97a040a24f665a2af4f3958
ismail ben ayed <ismailbenayed5@gmail.com>
2023-04-29 09:10:01 +0200

    added streamlit_audio_recorder

--- app.py
+++ app.py
@@ -4,11 +4,23 @@ from audio_recorder_streamlit import audio_recorder
 from PIL import Image
 import os
 import base64
+import numpy as np
+from io import BytesIO
+import streamlit.components.v1 as components
+from st_custom_components import st_audiorec
 
 
 def main():
     st.title("Simple Web App")
     st.write("Welcome to my simple web app!")
+    wav_audio_data = st_audiorec()
+
+    if wav_audio_data is not None:
+    # display audio data as received on the backend
+        st.audio(wav_audio_data, format='audio/wav')
+    
+# INFO: by calling the function an instance of the audio recorder is created
+# INFO: once a recording is completed, audio data will be saved to wav_audio_data
 
     # Get user input
     text_input = st.text_input("Enter some text:")
