d6a8ec7c823e6ddb457427d55b76ae93790d78f6
Mahdi Koiubaa <mahdi.koubaa@tum.de>
2023-04-30 03:02:17 +0200

    d

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import io
 import streamlit as st
 import tempfile
 from PIL import Image
@@ -9,10 +10,6 @@ import streamlit.components.v1 as components
 from st_custom_components import st_audiorec
 from reportlab.pdfgen import canvas
 from PyPDF2 import PdfFileMerger
-from reportlab.lib.pagesizes import letter
-from reportlab.lib.units import inch
-from reportlab.pdfgen import canvas
-from reportlab.lib import colors
 import speech2text as s2t
 import wave 
 
@@ -27,19 +24,27 @@ def main():
     st.write(speech)
     translatedspeech= s2t.translate_text(speech,"fr")
     st.write(translatedspeech)
-    
-    q_name = s2t.answer_question("what is the name of the speaker?", translatedspeech)
-    q_location = s2t.answer_question("where was the task done?", translatedspeech) 
-    q_task_name = s2t.answer_question("what is the name of the task?", translatedspeech) 
-    q_task_id = s2t.answer_question("what is the task id?", translatedspeech)
-    q_finished  = s2t.answer_question("was the task finished successfully?", translatedspeech)
-    q_process = s2t.answer_question("what is the process of the task?", translatedspeech)    
-    
-    st.write(q_task_name)
-    # st.write("what is the name of the speaker?")
-    # st.write(q_name)
+    q_name = "what is the name of the speaker?"
+    q_location = "where was the task done?"
+    q_task_name = "what is the name of the task?"
+    q_task_id = "what is the task id?"
+    q_finished  = "was the task finished successfully?"
+
+    q_process = "what is the process of the task?"
+
+
+    questions = [q_name, q_location, q_task_name, q_task_id, q_finished, q_process]
+    answers = dict()
+
+    for question in questions :
+        answers[question] = s2t.answer_question(question, translatedspeech)
+
+    for k,v in answers.items():
+        st.write("{} : {}".format(k , v))
     # INFO: by calling the function an instance of the audio recorder is created
     # INFO: once a recording is completed, audio data will be saved to wav_audio_data
+
+
 
     # Get user input
     text_input = st.text_input("Enter some text:")
@@ -60,6 +65,8 @@ def main():
     if st.session_state.camera_enabled:
         st.write("Taking picture...")
         img_file_buffer = st.camera_input("Take a picture")
+        image_bytes = img_file_buffer.read()
+        st.write(s2t.analyze_image(image_bytes))
 
     # Display saved image
     if st.session_state.img_file_buffer is not None:
@@ -71,71 +78,44 @@ def main():
     if file_input is not None:
         st.write("Image uploaded!")
         image = Image.open(file_input)
+        with io.BytesIO() as output:
+            image.save(output, format= image.format)
+            image_bytes = output.getvalue()
+        st.write(s2t.analyze_image(image_bytes))
         st.image(image, caption="Uploaded image", use_column_width=True)
 
     # Show user input
     if text_input:
         st.write("You entered:", text_input)
-    # Employee name
-    employee_name = q_name
 
-    # Location
-    location = q_location
-
-    # Task
-    task = q_task_name
-
-    # Task Finished
-    task_finished = q_finished
-
-    # Date
-    date = st.date_input("Date:")
-
-    # Description
-    description = q_process
-
-    # Media/Images
-    #media_images = st.file_uploader("Media/Images:")
-
-    # Generate PDF report using ReportLab
-    c = canvas.Canvas("employee_report.pdf", pagesize=letter)
-    # Set the font size and position of the title
-    c.setFont("Helvetica-Bold", 16)
-    title_x, title_y = 1*inch, 10.5*inch
-
-    # Draw the title on the first page
-    c.drawString(title_x, title_y, "Employee Report: Task Completion")
-    # Employee Information
-    c.setFont("Helvetica-Bold", 14)
-    c.drawString(100, 700, "Emplyee name:")
-    c.setFont("Helvetica", 12)
-    c.drawString(100, 680, "{}".format(q_name))
-    c.drawString(100, 660, "Location:")
-    c.drawString(100, 640, "{}".format(q_location))
-    
-    # Task Information
-    c.setFont("Helvetica-Bold", 14)
-    c.drawString(100, 620, "Task Information")
-    c.setFont("Helvetica", 12)
-    c.drawString(100, 600, "Task:")
-    c.drawString(100, 580, "{}".format(q_task_name))
-
-    c.showPage()
-    c.save()
+    pdf = canvas.Canvas("report.pdf")
+    pdf.setFont("Helvetica-Bold", 16)
+    pdf.drawString(100, 750, "Report")
+    pdf.setFont("Helvetica", 12)
+    pdf.drawString(100, 700, "Emplyee name:")
+    pdf.drawString(100, 680, "Location:")
+    pdf.drawString(100, 660, "Task:")
+    pdf.drawString(100, 640, "Task finished:")
+    pdf.drawString(100, 620, "Date:")
+    pdf.drawString(100, 600, "Description:")
+    pdf.drawString(100, 580, "Image:")
+    pdf.drawString(120, 560, text_input)
+    pdf.showPage()
+    pdf.save()
 
     # Stream generated PDF to user
     def download_report():
-        with open("employee_report.pdf", "rb") as f:
+        with open("report.pdf", "rb") as f:
             data = f.read()
         st.download_button(
             label="Download Report",
             data=data,
-            file_name="employee_report.pdf",
+            file_name="report.pdf",
             mime="application/pdf",
         )
 
-
     download_report()
+
 
 if __name__ == "__main__":
     main()
